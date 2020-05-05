92d97d5ca121351303c136a888bd5124e66e9ee3
Aron Watson <watsaro@gmail.com>
2020-05-05 08:42:33 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template_string
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+piglatin_url="https://hidden-journey-62459.herokuapp.com/piglatinize/"
+
+def rtn_template(url, fact):
+    rtn =  """<a href="{url}" target="_blank">Your Fact in Pig Latin</a> """
+    return render_template_string('<p>See the fact<br/>'
+                                  '{{fact}}<br/>'
+                                  'in Pig Latin<br/><a href="{{url}}" target="_blank">Click Here</a></p>', url = url, fact=fact)
 
 def get_fact():
 
@@ -19,7 +26,10 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    r = requests.post(piglatin_url, data={'input_text': fact})
+    link = rtn_template(r.url, fact)
+    return link
 
 
 if __name__ == "__main__":
